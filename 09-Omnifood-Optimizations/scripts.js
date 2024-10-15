83bf76cc7e56451832f2af245d641dc5c17b5f9d
Daumantas <daumis05@gmail.com>
2024-10-15 22:11:28 +0300

    Fixing Safari flexbox gap bug

--- 09-Omnifood-Optimizations/scripts.js
+++ 09-Omnifood-Optimizations/scripts.js
@@ -55,3 +55,25 @@ const observer = new IntersectionObserver((entries) => {
 })
 observer.observe(sectionHeroEl);
 
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions (also needs additional CSS for each element where gap is used. I've added code for this specific website in queries.css, but it would need to manually be made for each website)
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
+
