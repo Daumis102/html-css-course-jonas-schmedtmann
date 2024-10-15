dd77280240c20cd9cd95fb1a738b4380b6f81b45
Daumantas <daumis05@gmail.com>
2024-10-15 20:52:13 +0300

    Add open and close navigation functionality

--- 09-Omnifood-Optimizations/scripts.js
+++ 09-Omnifood-Optimizations/scripts.js
@@ -1,3 +1,12 @@
+// update copyright year
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector('.button-mobile-nav');
+
+btnNavEl.addEventListener("click", () => {
+    headerEl.classList.toggle('nav-open');
+})
